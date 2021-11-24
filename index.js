f299733929c59332e42e69be55e1ceedaf208abc
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-24 01:45:36 +0000

    add files, stuff

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const 
+const express = require('express')
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(process.env.PORT || 8080, function() {
+    console.log('listening on *:8080', process.env.PORT);
+});
