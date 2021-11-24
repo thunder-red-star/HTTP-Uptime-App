a227cd827658675a688ae0db9a2593324ab3f879
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-24 02:31:48 +0000

    working on server app

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-const express = require('express')
-const MemoryStore = require('memorystore')(session);
-const path = require('path')
+// a pile of imports, because why not
 
+const express = require('express')
+const app = express();
+const session = require("express-session");
+const ejs = require("ejs");
+const MemoryStore = require('memorystore')(session);
+const path = require('path');
+const bodyParser = require("body-parser");
+
+module.exports = () => {
+	const templateDir = path.resolve(`${dataDir}${path.sep}templates`);
+
+	app.use(session({
+		store: new MemoryStore({ checkPeriod: 86400000 }),
+		secret: "10993D1N",
+		resave: false,
+		saveUninitialized: false,
+	}));
+
+	app.engine("html", ejs.renderFile);
+	app.set("view engine", "html");
+
+	app.use(bodyParser.json());
+}
