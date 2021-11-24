2fb9357715abcfaa248deec993dfecb1c1df8141
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-24 01:46:01 +0000

    add package/package lock

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(process.env.PORT || 8080, function() {
+app.listen(process.env.PORT || 8080, function() {
     console.log('listening on *:8080', process.env.PORT);
 });
